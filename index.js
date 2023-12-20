6b84cade33bb3e62127e0d3419eaa0e84ef9dc7c
Kylie Clark <kyliedarlene@icloud.com>
2023-12-20 13:18:41 -0700

    first two tests passing

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        activity = "roller-skate";
+    }
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity) {
+    if (activity === undefined) {
+        activity = "go to the office";
+    }
+    return `This Monday, I will ${activity}.`
+}
